db00e306275b56dc1d55b198ab114596e0d05c6f
Takuya <takuya@okuyama-mac.local>
2020-03-07 11:17:28 +0900

    create swift project

---
+++ studySwift/studySwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  studySwift
+//
+//  Created by 奥山拓弥 on 2020/03/07.
+//  Copyright © 2020 奥山拓弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
